0e7de62a2a7580ec078909da16ee7abf634adc0d
KayleeHartzog <har15031@byui.edu>
2019-05-13 17:51:27 -0600

    tried using filter

--- cityBuilder.js
+++ cityBuilder.js
@@ -7,25 +7,62 @@ let input = fs.readFileSync('./citiesData.csv', "utf8")
 let cityArray = dsv.csvParse(input)
 
  //console.log(cityArray)
-//console.log(Object.keys(cityArray))
 // convert from 1 dimension to that other thing
-let doReduce = cityArray.reduce(function(accumulator, currentValue) {
-//  console.log("accumulator: ", accumulator)
-//  console.log("currentValue: ", currentValue)
 
-console.log("This is value: ", currentValue['Country Name'])
-  if (accumulator[currentValue['Country Name']] != undefined){ //undefined isn't goint to work, because Country Name will always have a value to it
-console.log("You made it!!\n")
-  }
-  else{
-    //newArray.push(currentValue)
-    accumulator.push(currentValue)
-    //console.log("Accumulator: ", newArray, "\n")
-  }
-  return accumulator
+let newArray = []
+
+const result1 = cityArray.filter(country => country['Country Name'] == "Country1")
+
+let doReduce = result1.reduce(function(accumulator, currentValue) {
+ if (accumulator.includes(currentValue['State Name']) === false) {
+    accumulator.push(currentValue.name = currentValue["State Name"])
+ }
+//console.log(currentValue['State Name'])
+accumulator.sort()
+
+let doReduce2 = accumulator.reduce(function(accumulator2, currentValue2){
+  
+}, {})
+return accumulator
 }, [])
 
-//console.log(doReduce)
+console.log(doReduce)
+
+
+// newArray.push(result1)
+
+// console.log(newArray)
+// console.log(cityArray[0]['Country Name'])
+
+
+
+
+
+
+
+
+
+
+
+
+// let doReduce = cityArray.reduce(function(accumulator, currentValue) {
+//   // console.log("accumulator: ", accumulator)
+//   // console.log("currentValue: ", currentValue)
+
+//  console.log("This is value: ", accumulator[currentValue['Country Name']])
+//   if (accumulator[currentValue['Country Name']] != undefined){ //undefined isn't goint to work, because Country Name will always have a value to it
+//   console.log("You made it!!\n")
+//   }
+//   else{
+//     //newArray.push(currentValue)
+//     accumulator.push(currentValue)
+//     //console.log("Accumulator: ", newArray, "\n")
+//     // console.log("Accumulator: ", accumulator)
+//   }
+//   return accumulator
+// }, [])
+
+// console.log(doReduce)
 
 // print the json
 
